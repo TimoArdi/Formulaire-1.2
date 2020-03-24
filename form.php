68200d94a94b12b2f38e37874eac5004d30504f9
TimoArdi <61334551+TimoArdi@users.noreply.github.com>
2020-03-24 17:54:12 +0100

    Add files via upload

--- form.php
+++ form.php
@@ -14,34 +14,34 @@ else {
 <form  method="post" action="" >
     <p>
         <label  for="nom">Nom :</label>
-        <input  type="text"  id="name"  name="name" required value="<?php if(isset($_POST['name'])) echo htmlentities($_POST['name']);?>">
+        <input  type="text"  id="name"  name="name" required  value="<?php if(isset($_POST['name'])) echo htmlentities($_POST['name']);?>">
     </p>
-    <p><?php if(isset($errors['name1'])) echo $errors['name1']; ?></p>
+    <p><?php echo $errors['name1']; ?></p>
 
     <p>
         <label  for="Prenom">Prenom :</label>
         <input  type="text"  id="firstname"  name="user_name" required value="<?php if(isset($_POST['user_name'])) echo htmlentities($_POST['user_name']);?>" >
     </p>
-    <p><?php if(isset($errors['user_name1'])) echo $errors['user_name1']; ?></p>
+    <p><?php  echo $errors['user_name1']; ?></p>
 
     <p>
         <label  for="courriel">Courriel :</label>
         <input  type="email"  id="courriel"  name="user_email" required value="<?php if(isset($_POST['user_email'])) echo htmlentities($_POST['user_email']);?>">
     </p>
-    <p><?php if(isset($errors['user_email1'])) echo $errors['user_email1']; ?></p>
+    <p><?php echo $errors['user_email1']; ?></p>
 
     <p>
     <label for="phone">Numero de telephone :</label>
 
     <input type="tel" id="phone" name="user_phone" required value="<?php if(isset($_POST['user_phone'])) echo htmlentities($_POST['user_phone']);?>">
     </p>
-    <p><?php if(isset($errors['user_phone1'])) echo $errors['user_phone1']; ?></p>
+    <p><?php echo $errors['user_phone1']; ?></p>
 
     <p>
         <label  for="message">Message :</label>
         <textarea  id="message"  name="user_message" required><?php if(isset($_POST['user_message'])) echo htmlentities($_POST['user_message']);?></textarea>
     </p>
-    <p><?php if(isset($errors['user_message1'])) echo $errors['user_message1']; ?></p>
+    <p><?php echo $errors['user_message1']; ?></p>
 
     <p>
         <select name="user_reasons" id="reasons-select" required >
@@ -50,7 +50,7 @@ else {
             <option value="3">Question Produit</option>
         </select>
     </p>
-    
+
     <div  class="button">
         <button  type="submit">Envoyer votre message</button>
     </div>
